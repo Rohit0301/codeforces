152c548f6910db132f2f9eb4448ed5ddcb99379e
Rohit Jain <rj03012002@gmail.com>
2020-09-13 13:34:00 +0530

    Add solution for problem `A - Chat room` Link: https://codeforces.com/contest/58/problem/A Tags: greedy, strings, *1000 Ref: https://codeforces.com/contest/58/submission/92694218

---
+++ codeforces/58/A.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class chat_room  {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String b="hello";
+        int j=0;
+        for(int i=0;i<s.length();i++){
+             if(s.charAt(i)==b.charAt(j))
+                j++;
+            if(j==5)
+                break;
+        }
+        if(j==5)
+            System.out.println("YES");
+        else System.out.println("NO");
+    }
+    
+}
