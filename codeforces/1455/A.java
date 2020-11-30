e47c9399671ba8ca67272e58beba941b0ee6e88f
Rohit Jain <rj03012002@gmail.com>
2020-11-30 20:12:00 +0530

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/100000677

---
+++ codeforces/1455/A.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class cf{
+
+  
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+
+    int t=sc.nextInt();
+    while(t-->0){
+      String s=sc.next();
+      System.out.println(s.length());
+    }
+       
+  }
+}
