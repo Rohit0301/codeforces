9b92596cf6a4174c1307ed5cdbde7f58cddadfbf
Rohit Jain <rj03012002@gmail.com>
2021-01-19 20:29:00 +0530

    Add solution for problem `B - Different Divisors` Link: https://codeforces.com/contest/1474/problem/B Tags: binary search, constructive algorithms, greedy, math, number theory Ref: https://codeforces.com/contest/1474/submission/104788647

---
+++ codeforces/1474/B.java
@@ -0,0 +1,83 @@
+/*
+
+    /$$$$$  /$$$$$$  /$$    /$$  /$$$$$$                                                                  
+   |__  $$ /$$__  $$| $$   | $$ /$$__  $$                                                                 
+      | $$| $$  \ $$| $$   | $$| $$  \ $$                                                                 
+      | $$| $$$$$$$$|  $$ / $$/| $$$$$$$$                                                                 
+ /$$  | $$| $$__  $$ \  $$ $$/ | $$__  $$                                                                 
+| $$  | $$| $$  | $$  \  $$$/  | $$  | $$                                                                 
+|  $$$$$$/| $$  | $$   \  $/   | $$  | $$                                                                 
+ \______/ |__/  |__/    \_/    |__/  |__/                                                                 
+ /$$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$  /$$      /$$ /$$      /$$ /$$$$$$$$ /$$$$$$$ 
+| $$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$| $$$    /$$$| $$$    /$$$| $$_____/| $$__  $$
+| $$  \ $$| $$  \ $$| $$  \ $$| $$  \__/| $$  \ $$| $$  \ $$| $$$$  /$$$$| $$$$  /$$$$| $$      | $$  \ $$
+| $$$$$$$/| $$$$$$$/| $$  | $$| $$ /$$$$| $$$$$$$/| $$$$$$$$| $$ $$/$$ $$| $$ $$/$$ $$| $$$$$   | $$$$$$$/
+| $$____/ | $$__  $$| $$  | $$| $$|_  $$| $$__  $$| $$__  $$| $$  $$$| $$| $$  $$$| $$| $$__/   | $$__  $$
+| $$      | $$  \ $$| $$  | $$| $$  \ $$| $$  \ $$| $$  | $$| $$\  $ | $$| $$\  $ | $$| $$      | $$  \ $$
+| $$      | $$  | $$|  $$$$$$/|  $$$$$$/| $$  | $$| $$  | $$| $$ \/  | $$| $$ \/  | $$| $$$$$$$$| $$  | $$
+|__/      |__/  |__/ \______/  \______/ |__/  |__/|__/  |__/|__/     |__/|__/     |__/|________/|__/  |__/
+                                                                                                          
+                                                                                                          
+
+*/
+
+import java.io.*;
+import java.util.*;
+public class cf
+{
+   static class pair{
+    long x;
+    Long y;
+    public pair(long x,long y){
+        this.x=x;
+        this.y=y;
+    }
+   }
+   static boolean prime[];
+  static  void prime(int n) 
+    { 
+      
+        prime= new boolean[n+1]; 
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        { 
+            // If prime[p] is not changed, then it is a prime 
+            if(prime[p] == true) 
+            { 
+                // Update all multiples of p 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+        
+    } 
+
+      
+    public static void main(String args[])
+    {
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      Scanner sc=new Scanner(System.in);
+     prime(100000);
+       int t=sc.nextInt();
+       while(t-->0){
+         int d=sc.nextInt();
+         int x=1;
+         x+=d;
+          int ans=1;
+           while(prime[x]!=true){
+            x++;
+           }
+           ans*=x;
+           x+=d;
+           while(prime[x]!=true){
+            x++;
+           }
+           ans*=x;
+           System.out.println(ans);
+
+       }
+
+   }
+}
