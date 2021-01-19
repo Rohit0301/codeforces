dbf9d3aad3b77e23ac62dccc1e56ffd2101522d3
Rohit Jain <rj03012002@gmail.com>
2021-01-19 20:48:00 +0530

    Add solution for problem `A - Puzzle From the Future` Link: https://codeforces.com/contest/1474/problem/A Tags: greedy Ref: https://codeforces.com/contest/1474/submission/104801619

---
+++ codeforces/1474/A.java
@@ -0,0 +1,89 @@
+/*
+
+    /$$$$$  /$$$$$$  /$$    /$$  /$$$$$$                                                                  
+   |__  $$ /$$__  $$| $$   | $$ /$$__  $$                                                                 
+      | $$| $$  \ $$| $$   | $$| $$  \ $$                                                                 
+      | $$| $$$$$$$$|  $$ / $$/| $$$$$$$$                                                                 
+ /$$  | $$| $$__  $$ \  $$ $$/ | $$__  $$                                                                 
+| $$  | $$| $$  | $$  \  $$$/  | $$  | $$                                                                 
+|  $$$$$$/| $$  | $$   \  $/   | $$  | $$                                                                 
+ \______/ |__/  |__/    \_/    |__/  |__/                                                                 
+ /$$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$  /$$      /$$ /$$      /$$ /$$$$$$$$ /$$$$$$$ 
+| $$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$| $$$    /$$$| $$$    /$$$| $$_____/| $$__  $$
+| $$  \ $$| $$  \ $$| $$  \ $$| $$  \__/| $$  \ $$| $$  \ $$| $$$$  /$$$$| $$$$  /$$$$| $$      | $$  \ $$
+| $$$$$$$/| $$$$$$$/| $$  | $$| $$ /$$$$| $$$$$$$/| $$$$$$$$| $$ $$/$$ $$| $$ $$/$$ $$| $$$$$   | $$$$$$$/
+| $$____/ | $$__  $$| $$  | $$| $$|_  $$| $$__  $$| $$__  $$| $$  $$$| $$| $$  $$$| $$| $$__/   | $$__  $$
+| $$      | $$  \ $$| $$  | $$| $$  \ $$| $$  \ $$| $$  | $$| $$\  $ | $$| $$\  $ | $$| $$      | $$  \ $$
+| $$      | $$  | $$|  $$$$$$/|  $$$$$$/| $$  | $$| $$  | $$| $$ \/  | $$| $$ \/  | $$| $$$$$$$$| $$  | $$
+|__/      |__/  |__/ \______/  \______/ |__/  |__/|__/  |__/|__/     |__/|__/     |__/|________/|__/  |__/
+                                                                                                          
+                                                                                                          
+
+*/
+
+import java.io.*;
+import java.util.*;
+public class cf
+{
+   // static class pair{
+   //  long x;
+   //  Long y;
+   //  public pair(long x,long y){
+   //      this.x=x;
+   //      this.y=y;
+   //  }
+   // }
+   static boolean prime[];
+  static  void prime(int n) 
+    { 
+      
+        prime= new boolean[n+1]; 
+        for(int i=0;i<n;i++) 
+            prime[i] = true; 
+          
+        for(int p = 2; p*p <=n; p++) 
+        { 
+            // If prime[p] is not changed, then it is a prime 
+            if(prime[p] == true) 
+            { 
+                // Update all multiples of p 
+                for(int i = p*p; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+        
+    } 
+
+      
+    public static void main(String args[])
+    {
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      Scanner sc=new Scanner(System.in);
+     prime(100000);
+       int t=sc.nextInt();
+       while(t-->0){
+        int n=sc.nextInt();
+        String s=sc.next();
+        char ans[]=new char[100001];
+        ans[0]='1';
+        int k=s.charAt(0)+1;
+        char p=(char)k;
+        for(int i=0;i<n;i++){
+           if(s.charAt(i)+'1'!=p){
+            ans[i]='1';
+           }   
+           else{
+            ans[i]='0';
+           }
+           k=s.charAt(i)+ans[i];
+           p=(char)k;
+        }
+        for(int i=0;i<n;i++){
+          System.out.print(ans[i]);   
+        }
+        System.out.println();
+
+       }
+
+   }
+}
