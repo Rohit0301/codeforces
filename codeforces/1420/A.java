8d9a5ac9fdcf1ac2888512e266631be0a55bfd83
Rohit Jain <rj03012002@gmail.com>
2021-01-21 23:19:00 +0530

    Add solution for problem `A - Cubes Sorting` Link: https://codeforces.com/contest/1420/problem/A Tags: math, sortings, *900 Ref: https://codeforces.com/contest/1420/submission/105011639

---
+++ codeforces/1420/A.java
@@ -0,0 +1,83 @@
+/*
+
+    /$$$$$  /$$$$$$  /$$    /$$  /$$$$$$                                                                  
+   |__  $$ /$$__  $$| $$   | $$ /$$__  $$                                                                 
+      | $$| $$  \ $$| $$   | $$| $$  \ $$                                                                 
+      | $$| $$$$$$$$|  $$ / $$/| $$$$$$$$                                                                 
+ /$$  | $$| $$__  $$ \  $$ $$/ | $$__  $$                                                                 
+| $$  | $$| $$  | $$  \  $$$/  | $$  | $$                                                                 
+|  $$$$$$/| $$  | $$   \  $/   | $$  | $$                                                                 
+ \______/ |__/  |__/    \_/    |__/  |__/                                                                 
+ /$$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$  /$$      /$$ /$$      /$$ /$$$$$$$$ /$$$$$$$ 
+| $$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$__  $$ /$$__  $$| $$$    /$$$| $$$    /$$$| $$_____/| $$__  $$
+| $$  \ $$| $$  \ $$| $$  \ $$| $$  \__/| $$  \ $$| $$  \ $$| $$$$  /$$$$| $$$$  /$$$$| $$      | $$  \ $$
+| $$$$$$$/| $$$$$$$/| $$  | $$| $$ /$$$$| $$$$$$$/| $$$$$$$$| $$ $$/$$ $$| $$ $$/$$ $$| $$$$$   | $$$$$$$/
+| $$____/ | $$__  $$| $$  | $$| $$|_  $$| $$__  $$| $$__  $$| $$  $$$| $$| $$  $$$| $$| $$__/   | $$__  $$
+| $$      | $$  \ $$| $$  | $$| $$  \ $$| $$  \ $$| $$  | $$| $$\  $ | $$| $$\  $ | $$| $$      | $$  \ $$
+| $$      | $$  | $$|  $$$$$$/|  $$$$$$/| $$  | $$| $$  | $$| $$ \/  | $$| $$ \/  | $$| $$$$$$$$| $$  | $$
+|__/      |__/  |__/ \______/  \______/ |__/  |__/|__/  |__/|__/     |__/|__/     |__/|________/|__/  |__/
+                                                                                                          
+                                                                                                          
+
+*/
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class cf
+{
+   // static class pair{
+   //  long x;
+   //  Long y;
+   //  public pair(long x,long y){
+   //      this.x=x;
+   //      this.y=y;
+   //  }
+   // }
+
+  static int dp[][];
+    static int solve(String s1,String s2,int n,int m){
+      if(n<=0 || m<=0)
+        return 0;
+      if(dp[n][m]!=-1)
+        return dp[n][m];
+      if(s1.charAt(n-1)==s2.charAt(m-1)){
+        return dp[n][m]=solve(s1,s2,n-1,m-1)+2;
+      }
+      else{
+        return dp[n][m]= Math.max(solve(s1,s2,n-1,m)-1,solve(s1,s2,n,m-1)-1);
+      }
+    }
+    
+   
+    public static void main(String args[]) throws IOException
+    {
+      Scanner sc=new Scanner(System.in);
+     
+        int t=sc.nextInt();
+        while(t-->0){
+         int n=sc.nextInt();
+         int a[]=new int[n];
+         for(int i=0;i<n;i++){
+           a[i]=sc.nextInt();   
+         }
+
+        boolean can=false;
+        for (int i=1; i<n; i++)
+        {
+            if (a[i]>=a[i-1])
+            {
+                can=true;
+                break;
+            }
+        }
+        if (can) System.out.println("YES");
+        else System.out.println("NO");
+     }
+
+
+
+
+      
+}
+}
